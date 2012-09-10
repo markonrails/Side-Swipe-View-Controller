6a4aff9f3a203eff60baf036a4fe3563d6b8459b
Mark <cym1117@gmail.com>
2012-09-10 01:02:51 -0500

    Initial commit

---
+++ SideSwipe/SideSwipe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SideSwipe
+//
+//  Created by Yiming Chen on 9/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
